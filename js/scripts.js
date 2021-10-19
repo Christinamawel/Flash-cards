df87ff6dd92659a7ce0168e2adf3f2045f0db479
Christinamawel <christinamawel@gmail.com>
2021-10-19 15:46:42 -0700

    add jquery to page to create interactivity
    Co-authored-by: Karen Axon <kaxonwa@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,92 @@
+$ (document).ready(function() {
+  $("#js-card").click(function() {
+    $("#js").toggle();
+    $("#js-D").toggle();
+  });
+
+  $("#op-card").click(function() {
+    $("#op").toggle();
+    $("#op-D").toggle();
+  });
+
+  $("#var-card").click(function() {
+    $("#var").toggle();
+    $("#var-D").toggle();
+  });
+
+  $("#var-name-card").click(function() {
+    $("#var-name").toggle();
+    $("#var-name-D").toggle();
+  });
+
+  $("#functions-card").click(function() {
+    $("#functions").toggle();
+    $("#functions-D").toggle();
+  });
+
+  $("#methods-card").click(function() {
+    $("#methods").toggle();
+    $("#methods-D").toggle();
+  });
+
+  $("#arguments-card").click(function() {
+    $("#arguments").toggle();
+    $("#arguments-D").toggle();
+  });
+
+  $("#parameters-card").click(function() {
+    $("#parameters").toggle();
+    $("#parameters-D").toggle();
+  });
+
+  $("#return-card").click(function() {
+    $("#return").toggle();
+    $("#return-D").toggle();
+  });
+
+  $("#return-card").click(function() {
+    $("#return").toggle();
+    $("#return-D").toggle();
+  });
+
+  $("#chaining-card").click(function() {
+    $("#chaining").toggle();
+    $("#chaining-D").toggle();
+  });
+
+  $("#string-card").click(function() {
+    $("#string").toggle();
+    $("#string-D").toggle();
+  });
+
+  $("#boolean-card").click(function() {
+    $("#boolean").toggle();
+    $("#boolean-D").toggle();
+  });
+
+  $("#nan-card").click(function() {
+    $("#nan").toggle();
+    $("#nan-D").toggle();
+  });
+
+  $("#null-card").click(function() {
+    $("#null").toggle();
+    $("#null-D").toggle();
+  });
+
+  $("#alert-card").click(function() {
+    $("#alert").toggle();
+    $("#alert-D").toggle();
+  });
+
+  $("#comments-card").click(function() {
+    $("#comments").toggle();
+    $("#comments-D").toggle();
+  });
+
+  $("#jquery-card").click(function() {
+    $("#jquery").toggle();
+    $("#jquery-D").toggle();
+  });
+
+});
